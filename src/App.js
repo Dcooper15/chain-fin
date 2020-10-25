663d3757dd3a9913293508202d1852d91e79d376
Dylan Cooper <dylancooper155@gmail.com>
2020-10-25 19:03:33 -0400

    api configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    stockData: [],
+    
+  }
+
+  
+    
+    async componentDidMount() {
+      
+      const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=SPY&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=OCT`);
+      this.setState({ stockData: res.data, });
+      console.log(res)
+    }
+    
+
+
+  render() {
+    return(
+        <>
+          <h1>Hello CC Scanner</h1>
+
+        </>
+
+    )
+  }
+
 }
+
 
 export default App;
