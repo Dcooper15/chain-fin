8c99eb6e23ae2b0f7c3c8b5609d9b5f9996b1d73
Dylan Cooper <dylancooper155@gmail.com>
2020-10-26 14:06:18 -0400

    data rendering on app

--- src/App.js
+++ src/App.js
@@ -13,22 +13,36 @@ class App extends Component {
     async componentDidMount() {
       
       const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=SPY&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=OCT`);
-      this.setState({ stockData: res.data, });
+      this.setState({ stockData: [...this.state.stockData, res.data], });
       console.log(res)
     }
     
 
 
   render() {
+    
+    const { stockData } = this.state;
+   
+
+
     return(
-        <>
-          <h1>Hello CC Scanner</h1>
+      <>
+      <div>
+        <h1>Hello CC Scanner</h1>
+          {!!stockData.length ? ( stockData.map(option => (
+        
+        
+        <i key={option.id}>{option.symbol}, Stock Price: {option.underlyingPrice.toFixed(2)} Cost for 100 shares: {option.underlyingPrice * 100} </i>
+        ))) : (<p>loading data</p>)}
+      
 
-        </>
+          
 
-    )
+        </div>
+    </>
+    );
+
   }
-
 }
 
 
