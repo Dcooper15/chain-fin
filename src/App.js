ce67a181676f4491b123864a90bea70195425175
Dylan Cooper <dylancooper155@gmail.com>
2020-10-26 16:20:12 -0400

    implmented object.keys to render calls dynamically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import './App.css';
+import TechStocks from './components/TechStocks';
 
 class App extends Component {
   state = {
@@ -12,7 +13,7 @@ class App extends Component {
     
     async componentDidMount() {
       
-      const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=SPY&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=OCT`);
+      const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AAPL&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=OCT`);
       this.setState({ stockData: [...this.state.stockData, res.data] });
       console.log(res)
     }
@@ -34,13 +35,18 @@ class App extends Component {
           {!!stockData.length ? ( stockData.map(option => (
         
         
-        <i key={option.id}>{option.symbol}, Stock Price: {option.underlyingPrice.toFixed(2)} Cost for 100 shares: ${option.underlyingPrice * 100} ___ {option.callExpDateMap["2020-10-26:0"]["339.0"][0].ask} Premium collected: ${option.callExpDateMap["2020-10-26:0"]["339.0"][0].ask * 100}</i>
+        <i key={option.id}>{option.symbol}, Stock Price: {option.underlyingPrice.toFixed(2)} Cost for 100 shares: ${option.underlyingPrice * 100} ___ {Object.keys(option.callExpDateMap).map((entry) => {
+          return Object.keys(option.callExpDateMap[entry]).map((innerArrayID) => option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2));
+      })} Premium collected: ${Object.keys(option.callExpDateMap).map((entry) => {
+        return Object.keys(option.callExpDateMap[entry]).map((innerArrayID) => option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2) * 100);
+    })}</i>
         ))) : (<p>loading data...</p>)}
       
 
           
 
         </div>
+        <TechStocks />
     </>
     );
 
--- src/components/TechStocks.jsx
+++ src/components/TechStocks.jsx
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
 class TechStocks extends Component {
+    state = {
+        amdData: [],
+        
+      }
+
+
+    async componentDidMount() {
+      
+        const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AMD&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=OCT`);
+        this.setState({ amdData: [...this.state.amdData, res.data] });
+        console.log(res)
+      }
+      
+  
     render() {
-        return (
-            <div>
-                
+        const { amdData } = this.state;
+   
+
+
+        return(
+          <>
+          <div>
+            
+              {!!amdData.length ? ( amdData.map(option => (
+            
+            
+            <i key={option.id}>{option.symbol}, Stock Price: {option.underlyingPrice.toFixed(2)} Cost for 100 shares: ${option.underlyingPrice * 100} ___ {Object.keys(option.callExpDateMap).map((entry) => {
+                return Object.keys(option.callExpDateMap[entry]).map((innerArrayID) => option.callExpDateMap[entry][innerArrayID][0].ask);
+            })} Premium Collected: ${Object.keys(option.callExpDateMap).map((entry) => {
+                return Object.keys(option.callExpDateMap[entry]).map((innerArrayID) => option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2) * 100);
+            })}</i>
+            ))) : (<p>loading data...</p>)}
+          
+    
+              
+    
             </div>
-        )
-    }
+        </>
+        );
+}
 }
 
 
