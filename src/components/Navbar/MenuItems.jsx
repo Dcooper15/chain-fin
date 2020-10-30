fa4ada8247e4221aed17a5eaee9842dff13626e5
Dylan Cooper <dylancooper155@gmail.com>
2020-10-30 16:15:58 -0400

    test

--- src/components/Navbar/MenuItems.jsx
+++ src/components/Navbar/MenuItems.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
 export const MenuItems = [
     {
         title: 'Finance',
-        url: '/finance',
+        url:  "/finance",
         cName: 'nav-links'
     },
     {
