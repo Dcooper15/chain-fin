24a3f9881da1443a0ad1ef9b914e235270b21ed2
Dylan Cooper <dylancooper155@gmail.com>
2021-08-24 02:27:48 -0400

    refactored finance to hooks

--- src/components/Tech/Amd.jsx
+++ src/components/Tech/Amd.jsx
@@ -1,63 +1,68 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { Card } from "@material-ui/core";
 
-class Amd extends Component {
-    state = {
-        amdData: [],
-        
-      }
-
-      async componentDidMount() {
+const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AMD&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`
+function Amd() {
+    const [amd, setAmdData] = useState([]);
+       
+    
+      useEffect(() => {
+        axios.get(url).then((response) => {
+          setAmdData([response.data]);
+          console.log("response.data is...", response.data);
+        });
+      },[]
+      )
       
-        const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AMD&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`);
-        this.setState({ amdData: [...this.state.amdData, res.data] });
-        
-      }
-
-
-
-    render() {
-        const { amdData } = this.state;
-        
-        
-        return (
-            <div>
-                {/* AMD Data */}
+  
+  return(
+  <>
             
-              {!!amdData.length ? ( amdData.map(option => (
             
-            <Card className="stockInfo" variant="outlined" style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}><i><strong>Advanced Micro Devices</strong></i><hr></hr><i key={option.index}>
-            {option.symbol}</i><br></br><i>Stock Price:{" "}
-            ${option.underlyingPrice.toFixed(2)}</i><br></br><i> Cost for 100 shares: $
-            {option.underlyingPrice.toFixed(2) * 100}</i><br></br><i>Ask Price: $
+            
+    {!!amd.length ? ( amd.map(option => (
+      <Card className="stockInfo" variant="outlined"
+        style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}>
+          <i><strong>Advance Micro Devices</strong></i>
+            <hr></hr>
+            <i key={option.index}>
+            {option.symbol}</i>
+          <br></br>
+            <i>Stock Price:{" "}
+            ${option.underlyingPrice.toFixed(2)}</i>
+          <br></br>
+            <i> Cost for 100 shares: $
+            {option.underlyingPrice.toFixed(0) * 100}</i>
+          <br></br>
+            <i>Ask Price: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(
-                option.callExpDateMap[entry]
-              ).map((innerArrayID) =>
-                option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
-              );
+            return Object.keys(
+            option.callExpDateMap[entry]
+            ).map((innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
+            );
             })}{" "}</i>
-            <br></br><i>Premium collected: $
+          <br></br>
+            <i>Premium collected: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(option.callExpDateMap[entry]).map(
-                (innerArrayID) =>
-                  option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
-                    2
-                  ) * 100
-              );
+            return Object.keys(option.callExpDateMap[entry]).map(
+            (innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
+            2) * 100
+            );
             })}
-          </i></Card>
-        ))
-      ) : (
-        <p>loading data...</p>
-      )}
-            
-            </div>
-        )
-    }
-}
+            </i>
+      </Card>
+            ))
+          ) : (
+            <p>loading data...</p>
+          )} 
 
+  </>
+        );
+
+};
 
 
 export default Amd;
--- src/components/Tech/Zm.jsx
+++ src/components/Tech/Zm.jsx
@@ -1,64 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { Card } from "@material-ui/core";
 
-class Zm extends Component {
-    state = {
-        zmData: [],
-        
-      }
-
-      async componentDidMount() {
-      
-        const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=ZM&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`);
-        this.setState({ zmData: [...this.state.zmData, res.data] });
-        
-      }
+const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=ZM&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`
+function Zm() {
+    const [zm, setZmData] = useState([]);
+       
+    
+      useEffect(() => {
+        axios.get(url).then((response) => {
+          setZmData([response.data]);
+          console.log("response.data is...", response.data);
+        });
+      },[]
+      )
       
   
-    render() {
-       
-        const { zmData } = this.state;
-
-
-        return(
-          <> 
-            <div>
-            {/* Zoom Data */}
-        
-            {!!zmData.length ? ( zmData.map(option => (
+  return(
+  <>
             
-            <Card className="stockInfo" variant="outlined" style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}><i><strong>Zoom</strong></i><hr></hr><i key={option.index}>
-            {option.symbol}</i><br></br><i>Stock Price:{" "}
-            ${option.underlyingPrice.toFixed(2)}</i><br></br><i> Cost for 100 shares: $
-            {option.underlyingPrice.toFixed(2) * 100}</i><br></br><i>Ask Price: $
+            
+            
+    {!!zm.length ? ( zm.map(option => (
+      <Card className="stockInfo" variant="outlined"
+        style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}>
+          <i><strong>Zoom</strong></i>
+            <hr></hr>
+            <i key={option.index}>
+            {option.symbol}</i>
+          <br></br>
+            <i>Stock Price:{" "}
+            ${option.underlyingPrice.toFixed(2)}</i>
+          <br></br>
+            <i> Cost for 100 shares: $
+            {option.underlyingPrice.toFixed(0) * 100}</i>
+          <br></br>
+            <i>Ask Price: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(
-                option.callExpDateMap[entry]
-              ).map((innerArrayID) =>
-                option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
-              );
+            return Object.keys(
+            option.callExpDateMap[entry]
+            ).map((innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
+            );
             })}{" "}</i>
-            <br></br><i>Premium collected: $
+          <br></br>
+            <i>Premium collected: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(option.callExpDateMap[entry]).map(
-                (innerArrayID) =>
-                  option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
-                    2
-                  ) * 100
-              );
+            return Object.keys(option.callExpDateMap[entry]).map(
+            (innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
+            2) * 100
+            );
             })}
-          </i></Card>
-        ))
-      ) : (
-        <p>loading data...</p>
-      )}
-            </div>
-          
-        </>
+            </i>
+      </Card>
+            ))
+          ) : (
+            <p>loading data...</p>
+          )} 
+
+  </>
         );
-}
-}
+
+};
 
 
 export default Zm;
