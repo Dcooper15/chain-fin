59f3b732b5404bd7071bb29a6cb6144a1561bd1d
Dylan Cooper <dylancooper155@gmail.com>
2021-08-24 02:38:18 -0400

    refactored travel to hooks

--- src/components/Travel/Dal.jsx
+++ src/components/Travel/Dal.jsx
@@ -1,65 +1,68 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { Card } from "@material-ui/core";
 
-class Dal extends Component {
-    state = {
-        dalData: [],
-        
-      }
-
-      async componentDidMount() {
-      
-        const res = await axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=DAL&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`);
-        this.setState({ dalData: [...this.state.dalData, res.data] });
+const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=DAL&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`
+function Dal() {
+    const [dal, setDalData] = useState([]);
        
-      }
+    
+      useEffect(() => {
+        axios.get(url).then((response) => {
+          setDalData([response.data]);
+         
+        });
+      },[]
+      )
       
   
-    render() {
-       
-        const { dalData } = this.state;
-
-
-        return(
-          <> 
-            <div>
-            {/* Delta Data */}
-        
-            {!!dalData.length ? ( dalData.map(option => (
+  return(
+  <>
             
-            <Card className="stockInfo" variant="outlined" style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}><i><strong>Delta Airlines</strong></i><hr></hr><i key={option.index}>
-            {option.symbol}</i><br></br><i>Stock Price:{" "}
-            ${option.underlyingPrice.toFixed(2)}</i><br></br><i> Cost for 100 shares: $
-            {option.underlyingPrice.toFixed(2) * 100}</i><br></br><i>Ask Price: $
+            
+            
+    {!!dal.length ? ( dal.map(option => (
+      <Card className="stockInfo" variant="outlined"
+        style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}>
+          <i><strong>Delta Airlines</strong></i>
+            <hr></hr>
+            <i key={option.index}>
+            {option.symbol}</i>
+          <br></br>
+            <i>Stock Price:{" "}
+            ${option.underlyingPrice.toFixed(2)}</i>
+          <br></br>
+            <i> Cost for 100 shares: $
+            {option.underlyingPrice.toFixed(0) * 100}</i>
+          <br></br>
+            <i>Ask Price: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(
-                option.callExpDateMap[entry]
-              ).map((innerArrayID) =>
-                option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
-              );
+            return Object.keys(
+            option.callExpDateMap[entry]
+            ).map((innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(2)
+            );
             })}{" "}</i>
-            <br></br><i>Premium collected: $
+          <br></br>
+            <i>Premium collected: $
             {Object.keys(option.callExpDateMap).map((entry) => {
-              return Object.keys(option.callExpDateMap[entry]).map(
-                (innerArrayID) =>
-                  option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
-                    2
-                  ) * 100
-              );
+            return Object.keys(option.callExpDateMap[entry]).map(
+            (innerArrayID) =>
+            option.callExpDateMap[entry][innerArrayID][0].ask.toFixed(
+            2) * 100
+            );
             })}
-          </i></Card>
-        ))
-      ) : (
-        <p>loading data...</p>
-      )}
+            </i>
+      </Card>
+            ))
+          ) : (
+            <p>loading data...</p>
+          )} 
 
-            </div>
-          
-        </>
+  </>
         );
-}
-}
+
+};
 
 
 export default Dal;
