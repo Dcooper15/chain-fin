045d7d660e2f7b60711263e16fbccde713e38ae8
Dylan Cooper <dylancooper155@gmail.com>
2021-08-25 01:20:18 -0400

    added initial foreach refactor

--- src/components/Entertainment/Amc.jsx
+++ src/components/Entertainment/Amc.jsx
@@ -2,17 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { Card } from "@material-ui/core";
 
-const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AMC&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`
+const entArray = ['AMC', 'T', 'DIS', 'MGM', 'WYNN'];
+const symbols = [];
+entArray.forEach(symbol => symbols.push(symbol));
+
+console.log("Symbols are..", symbols)
+
+const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=AMC&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM` 
+
+
 function Amc() {
     const [amc, setAmcData] = useState([]);
+    const [t, setTData] = useState([]);
+    const [dis, setDisData] = useState([]);
+    const [mgm, setMgmData] = useState([]);
+    const [wynn, setWynnData] = useState([]);
        
-    
+    console.log("AMC issss..", amc);
       useEffect(() => {
-        axios.get(url).then((response) => {
-          setAmcData([response.data]);
+        
+       entArray.forEach(symbol => 
+        axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=${symbol}&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM` 
+        ).then((response) => {
+          if(symbol == 'AMC'){
+            setAmcData([response.data]);
+          } else if (symbol == 'T') {
+            setTData([response.data])
+          }
+          else if (symbol == 'DIS') {
+            setDisData([response.data])
+          }
+          else if (symbol == 'MGM') {
+            setMgmData([response.data])
+          }
+          else if (symbol == 'WYNN') {
+            setWynnData([response.data])
+          }
           
-        });
+         
+        }))
+        
       },[]
+        
       )
       
   
