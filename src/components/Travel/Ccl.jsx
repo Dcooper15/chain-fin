4b639598ad5facb5c8f3c83ff0f7577351143b04
Dylan Cooper <dylancooper155@gmail.com>
2021-08-25 02:45:51 -0400

    added api logic to travel

--- src/components/Travel/Ccl.jsx
+++ src/components/Travel/Ccl.jsx
@@ -2,17 +2,43 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { Card } from "@material-ui/core";
 
-const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=CCL&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM`
+const travelArray = ['CCL', 'DAL', 'LUV', 'NCLH', 'UAL'];
+
 function Ccl() {
     const [ccl, setCclData] = useState([]);
-       
+    const [dal, setDalData] = useState([]);
+    const [luv, setLuvData] = useState([]);
+    const [nclh, setNclhData] = useState([]);
+    const [ual, setUalData] = useState([]);
+ 
+    const dataArray = [ccl, dal, luv, nclh, ual];
+    
     
       useEffect(() => {
-        axios.get(url).then((response) => {
-          setCclData([response.data]);
+        
+       travelArray.map(symbol => 
+        axios.get(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${process.env.REACT_APP_GITHUB_CLIENT_ID}&symbol=${symbol}&contractType=CALL&strikeCount=1&optionType=CALL&expMonth=${process.env.REACT_APP_MONTH}&toDate=${process.env.REACT_APP_DATE}&range=OTM` 
+        ).then((response) => {
+          if(symbol === 'CCL'){
+            setCclData([response.data]);
+          } else if (symbol === 'DAL') {
+            setDalData([response.data])
+          }
+          else if (symbol === 'LUV') {
+            setLuvData([response.data])
+          }
+          else if (symbol === 'NCLH') {
+            setNclhData([response.data])
+          }
+          else if (symbol === 'UAL') {
+            setUalData([response.data])
+          }
+          
          
-        });
+        }))
+        
       },[]
+        
       )
       
   
@@ -21,21 +47,21 @@ function Ccl() {
             
             
             
-    {!!ccl.length ? ( ccl.map(option => (
+    {!!dataArray.length ? ( dataArray.map(stock => stock.map(option => (
       <Card className="stockInfo" variant="outlined"
         style={{backgroundColor: "#6d76f7", color: '#fff', borderRadius: '15px'}}>
-          <i><strong>Carnival Cruise Line</strong></i>
-            <hr></hr>
-            <i key={option.index}>
-            {option.symbol}</i>
+          {/* <i><strong>{name}</strong></i> */}
+            {/* <hr></hr> */}
+            <i key={1}>
+           <strong>{option.symbol}</strong></i>
           <br></br>
-            <i>Stock Price:{" "}
+            <i key={2}>Stock Price:{" "}
             ${option.underlyingPrice.toFixed(2)}</i>
           <br></br>
-            <i> Cost for 100 shares: $
+            <i key={3}> Cost for 100 shares: $
             {option.underlyingPrice.toFixed(0) * 100}</i>
           <br></br>
-            <i>Bid Price: $
+            <i key={4}>Bid Price: $
             {Object.keys(option.callExpDateMap).map((entry) => {
             return Object.keys(
             option.callExpDateMap[entry]
@@ -44,7 +70,7 @@ function Ccl() {
             );
             })}{" "}</i>
           <br></br>
-            <i>Premium collected: $
+            <i key={5}>Premium collected: $
             {Object.keys(option.callExpDateMap).map((entry) => {
             return Object.keys(option.callExpDateMap[entry]).map(
             (innerArrayID) =>
@@ -54,7 +80,7 @@ function Ccl() {
             })}
             </i>
       </Card>
-            ))
+            )))
           ) : (
             <p>loading data...</p>
           )} 
